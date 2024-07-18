20801a0170df0eddcd40987dafc65a3c899d40a1
Ningrui Zhang <ningrui1227@gmail.com>
2024-07-18 15:36:27 +0100

    add program_154, program_155

---
+++ program_154/program_154.js
@@ -0,0 +1,9 @@
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+function greetingTo() {
+    console.log(greeting + ", " + name + "!")
+}
+
+greetingTo()
