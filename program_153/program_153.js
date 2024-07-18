1188dc22e1fc35a64e0e2e7be382804efb500b18
Ningrui Zhang <ningrui1227@gmail.com>
2024-07-18 15:34:34 +0100

    add program_153

---
+++ program_153/program_153.js
@@ -0,0 +1,7 @@
+// Create two variables, firstName and lastName
+let firstName = "Ningrui"
+let lastName = "Zhang"
+// Concatenate the two variables into a third variable called fullName
+let fullName = firstName + " " + lastName
+// Log fullName to the console
+console.log(fullName)
