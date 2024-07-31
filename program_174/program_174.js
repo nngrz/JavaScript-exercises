28a8ac4f2ede4641c4d069acc0133d8c881f8e40
Ningrui Zhang <ningrui1227@gmail.com>
2024-07-31 12:10:13 +0100

    add program_174.js

--- program_174/program_174.js
+++ program_174/program_174.js
@@ -9,9 +9,16 @@
 
 // Use both a for loop and a template string to solve the challenge
 function generateSentence(desc, arr) {
+    let baseString = `The ${arr.length} ${desc} are ` 
+    const lastIndex = arr.length - 1
     for (let i = 0; i < arr.length; i++) {
-        return sentence = `The 3 largest countries are ${arr[i]}`
+        if (i === lastIndex) {
+            baseString += arr[i]
+        } else {
+            baseString += arr[i] + ", "
+        }
     }
+    return baseString
 }
 
-console.log(generateSentence(largest countries, ["China", "India", "USA"]))
+console.log(generateSentence("largest countries", ["China", "India", "USA"]))
